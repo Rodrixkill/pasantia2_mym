0cb2d6c9626deae26db08df2402c62e6fe76779d
Mendez9896 <andresmendez9896@gmail.com>
2021-01-30 19:23:00 -0400

    Datos

--- src/database.ts
+++ src/database.ts
@@ -2,10 +2,10 @@ import { createPool, Pool } from 'mysql2/promise'
 
 export async function connect(): Promise<Pool> {
     const connection = await createPool({
-        host: 'localhost',
-        user: 'root',
-        password: 'pass',
-        database: 'node_mysql_ts',
+        host: 'mysql.sistema.mym.com.bo',
+        user: 'mymcombo',
+        password: 'vkLY!Wzh',
+        database: 'mysqlsistema',
         connectionLimit: 10
     });
     return connection;
